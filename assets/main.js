7d034070df7607b0e5256eb67978eefabf2b1f27
Anewh <i0ianrusi0i@gmail.com>
2023-02-12 00:21:27 +0300

    4 practice

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+// import Vue from './vue';
+import App from './App.vue';
+
+import './main.scss';
+
+//import './assets/main.css'
+
+createApp(App).mount('#app')
+
+
+// start the Stimulus application
+
+
+// new Vue({
+//     render: (h) => h(App),
+// }).$mount('#app');
